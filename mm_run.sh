ef50015eee2ef279261ac26d2b7daa2638c74f4c
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-08-17 06:25:00 +0000

    new run shell

--- mm_run.sh
+++
@@ -1,5 +0,0 @@
-ulimit -n 4096
-export NGPUS=4
-export NNODES=2
-export MASTER_ADDR='10.138.15.192'
-python -m torch.distributed.launch --nproc_per_node=$NGPUS --nnodes=$NNODES --node_rank=0  --master_addr=$MASTER_ADDR --master_port=1234 tools/train_net.py --config-file "ty_tools/bdd_faster_rcnn_fbnet_domain_time_1x_b64.yaml" DTYPE "float16"
--- mm_run5.sh
+++
@@ -1,5 +0,0 @@
-ulimit -n 4096
-export NGPUS=4
-export NNODES=2
-export MASTER_ADDR='10.138.15.204'
-python -m torch.distributed.launch --nproc_per_node=$NGPUS --nnodes=$NNODES --node_rank=1  --master_addr=$MASTER_ADDR --master_port=1234 tools/train_net.py --config-file "configs/bdd_faster_rcnn_fbnet_lr2x_aug.yaml"
